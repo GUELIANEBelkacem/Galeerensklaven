0fcdf4b04f7c23bdeb3433008bb9b32515fa62dc
moham <moham@LAPTOP-PP2E8H0C>
2021-02-18 08:17:01 +0100

    jarcorrect

---
+++ Galeerensklaven/src/cps/node/NodeCI.java
@@ -0,0 +1,7 @@
+package cps.node;
+
+import fr.sorbonne_u.components.interfaces.OfferedCI;
+
+public interface NodeCI extends OfferedCI {
+
+}
--- Galeerensklaven/src/interfaces/Test3.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public class Test3 {
-<<<<<<< HEAD
-	double x;
-=======
->>>>>>> branch 'master' of https://github.com/GUELIANEBelkacem/Galeerensklaven.git
-
-}
