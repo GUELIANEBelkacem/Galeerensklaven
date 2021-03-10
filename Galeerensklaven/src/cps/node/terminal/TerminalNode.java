4e4d9db7945f8d63cb37c4acc07dfa3163a53213
3502950 <>
2021-03-10 21:08:00 +0100

    modifs for test

--- Galeerensklaven/src/cps/node/terminal/TerminalNode.java
+++ Galeerensklaven/src/cps/node/terminal/TerminalNode.java
@@ -17,31 +17,42 @@ import cps.communication.CommunicationInboundPort;
 import cps.communication.CommunicationOutboundPort;
 import cps.info.ConnectionInfo;
 import cps.info.address.AddressI;
+import cps.info.address.NodeAddress;
+import cps.info.position.Position;
 import cps.info.position.PositionI;
 import cps.message.MessageI;
+import cps.node.routing.RoutingNode;
 import cps.info.address.NodeAddressI;
 
 @RequiredInterfaces(required = { CommunicationCI.class, RegistrationCI.class })
 @OfferedInterfaces(offered = { CommunicationCI.class })
 
 public class TerminalNode extends AbstractComponent {
-	private NodeAddressI address;
-	private PositionI initialPosition;
-	private double initialRange;
+	//private NodeAddressI address;
+	//private PositionI initialPosition;
+	//private double initialRange;
 
 	//protected CommunicationCI comm;
 	protected CommunicationInboundPort tcip; 
 	protected RegistrationOutboundPort trop;
 	protected HashMap<AddressI, CommunicationOutboundPort> addressComOPmap;
 	
+	
+	
+	Random rand = new Random();
+	private double initialRange = 10;
+	private final NodeAddressI address= new NodeAddress(RoutingNode.genAddresse()) ;
+	private Position initialPosition = new Position(rand.nextInt(50), rand.nextInt(50));   // change this genPos
+	//private ConnectionInfo conInfo = new ConnectionInfo(this.address, ComIP_URI, RotIP_URI, true, pos, false);
+	
 
 	private Set<ConnectionInfo> voisins = new HashSet<>();
-
-	public TerminalNode(NodeAddressI address, PositionI initialPosition, double initialRange) throws Exception {
+	//public TerminalNode(NodeAddressI address, PositionI initialPosition, double initialRange) throws Exception {
+	public TerminalNode() throws Exception {
 		super(1, 0);
-		this.address = address;
-		this.initialPosition = initialPosition;
-		this.initialRange = initialRange;
+		//this.address = address;
+		//this.initialPosition = initialPosition;
+		//this.initialRange = initialRange;
 
 		this.trop = new RegistrationOutboundPort(RegistrationOutboundPort.generatePortURI(), this);
 		this.trop.publishPort();
