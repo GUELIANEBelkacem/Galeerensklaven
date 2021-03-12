e19ef3272e4bb28c65124ae2b26f3be19a96d6a0
3502950 <>
2021-03-12 14:59:57 +0100

    accesspoint underway

--- Galeerensklaven/src/cps/node/accesspoint/AccessPoint.java
+++ Galeerensklaven/src/cps/node/accesspoint/AccessPoint.java
@@ -2,9 +2,11 @@ package cps.node.accesspoint;
 
 import java.util.HashMap;
 import java.util.Random;
+import java.util.Map.Entry;
 
 import cps.communication.CommunicationInboundPort;
 import cps.communication.CommunicationOutboundPort;
+import cps.connecteurs.CommunicationConnector;
 import cps.info.address.AddressI;
 import cps.info.address.NodeAddress;
 import cps.info.address.NodeAddressI;
@@ -40,21 +42,68 @@ public class AccessPoint extends AbstractComponent implements NodeI {
 		// TODO Auto-generated constructor stub
 	}
 
+	/*
+	
 	public AccessPoint(String reflectionInboundPortURI, int nbThreads, int nbSchedulableThreads) {
 		super(reflectionInboundPortURI, nbThreads, nbSchedulableThreads);
 		// TODO Auto-generated constructor stub
 	}
 
+
+*/
 	@Override
-	public void connect(NodeAddressI address, String communicationInboundPortURI) throws Exception {
+	public void connect(NodeAddressI naddress, String communicationInboundPortURI) throws Exception {
 		// TODO Auto-generated method stub
+		try {
+			if (!addressComOPmap.containsKey(naddress)) {
+				CommunicationOutboundPort tempPort = new CommunicationOutboundPort(CommunicationOutboundPort.generatePortURI(), this);
+				tempPort.publishPort();
+				addressComOPmap.put(naddress,
+						tempPort);
+				this.doPortConnection(tempPort.getPortURI(), communicationInboundPortURI, CommunicationConnector.class.getCanonicalName());
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 
 	}
 
 	@Override
 	public void transmitMessage(MessageI m) throws Exception {
 		// TODO Auto-generated method stub
+		if(m.getAddress().isequalsAddress(this.address)) {
+			this.logMessage(this.address.getAddress() + " <===== " + m.getContent().getMessage());
+		}
+		else {
+			if(m.stillAlive()) {
 
+				m.decrementHops();
+				for (Entry<AddressI, CommunicationOutboundPort> e : addressComOPmap.entrySet()) {
+					try {
+						e.getValue().transmitMessage(m);
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public int hasRouteFor(AddressI address) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void ping() throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
